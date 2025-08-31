c85eecce7aa510319e530a10461fd3282914c312
Tamás Sólyomi <bgwwsc@inf.elte.hu>
2025-08-31 20:02:10 +0200

    Deleted car data since there is nothing to modify on that, just helps to keep track of active listings and dates

--- data/get_csvs.py
+++ data/get_csvs.py
@@ -5,13 +5,8 @@ import os
 
 load_dotenv("shhh.env")
 engine_connection = os.getenv("SQL_ENGINE")
-
 engine = create_engine(engine_connection)
 
 # Car Details Table
 car_details_df = pd.read_sql_query("SELECT * FROM car_details", engine)
-car_details_df.to_csv('data/car_details.csv', index=False)
-
-# Car Data Table
-car_data_df = pd.read_sql_query("SELECT * FROM car_data", engine)
-car_data_df.to_csv('data/car_data.csv', index=False)
+car_details_df.to_csv('data/raw/car_details.csv', index=False)
