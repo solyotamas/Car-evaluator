6f290edd8ffb4260ded2b992b90c85e405ac80d0
Tamás Sólyomi <bgwwsc@inf.elte.hu>
2025-08-31 20:01:25 +0200

    Testing different models, exploring best options, finetuning parameters

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
+import torch
 from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
 from nn.dataset import CarPriceDataset
-from nn.model import MLPCarPriceRegressionNet
+from nn.model import MLPCarPriceRegressionNet_V1
 import pandas as pd
 import pandas as pd
-from data_preprocessing.preprocessor import PreProcessor
+from preprocessing.preprocess import PreProcessor
+from nn.model_functions import train_model, plot_training, plot_error_analysis, plot_outlier_years, plot_outlier_km
 
 
 df = pd.read_csv('data/clean/car_details.csv')
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 df_train, df_temp = train_test_split(df, test_size=0.2, random_state=42)
-df_val, df_test = train_test_split(df_temp, test_size=0.25, random_state=42)
+df_val, df_test = train_test_split(df_temp, test_size=0.5, random_state=42)
 
 preprocessor = PreProcessor()
 preprocessor.fit(df_train)
 preprocessor.save('config/preprocessor_config.pkl', 'config/preprocessor_config.json')
 
-model = MLPCarPriceRegressionNet('config/preprocessor_config.pkl')
+model = MLPCarPriceRegressionNet_V1('config/preprocessor_config.pkl')
 
 X_num_train, X_cat_train, y_train = preprocessor.transform(df_train, is_training = True, include_target = True)
 X_num_val, X_cat_val, y_val = preprocessor.transform(df_val, is_training = False, include_target = True)
@@ -32,3 +34,20 @@ dataloader_val = DataLoader(dataset = val_dataset, batch_size = 64, shuffle = Tr
 dataloader_test = DataLoader(dataset = test_dataset, batch_size = 64, shuffle = False)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+'''
+history, model = train_model(
+    model=model,
+    train_loader=dataloader_train,
+    val_loader=dataloader_val,
+    epochs=150,
+    learning_rate=0.0005
+)
+torch.save(model.state_dict(), 'models/car_price_model.pt')
+'''
+#plot_training(history=history)
+
+model.load_state_dict(torch.load('models/car_price_model.pt'))
+#plot_error_analysis(model, dataloader_test)
+plot_outlier_years(model, dataloader_test, df_test, threshold=50)
+plot_outlier_km(model, dataloader_test, df_test, threshold=50)
--- nn/model.py
+++ nn/model.py
@@ -2,56 +2,76 @@ import torch
 import torch.nn as nn
 import joblib
 
-class MLPCarPriceRegressionNet(nn.Module):
+class MLPCarPriceRegressionNet_V1(nn.Module):
     def __init__(self, preprocessor_path):
         super().__init__()
         
-        # Load preprocessor config
         config = joblib.load(preprocessor_path)
         self.numerical_features = config['numerical_features']
         self.categorical_features = config['categorical_features']
+        self.categorical_order = list(self.categorical_features.keys())
         
-        # Build embedding layers from config
+        ''' 
+        self.embed_manufacturer = nn.Embedding(72, 36)
+        self.embed_fuel_type = nn.Embedding(8, 4)
+        self.embed_condition = nn.Embedding(6, 3)
+        self.embed_body_type = nn.Embedding(14, 7)
+        self.embed_color = nn.Embedding(39, 20)
+        self.embed_drive_type = nn.Embedding(7, 4)
+        self.embed_transmission_type = nn.Embedding(7, 4)
+        '''
         self.embeddings = nn.ModuleDict()
-        total_embed_dim = 0
-        
-        for feat_name, feat_config in self.categorical_features.items():
-            vocab_size = feat_config['vocab_size']
-            embed_dim = feat_config['embed_dim']
+        embeddings_dim = 0
+        for feature_name, feature_config in self.categorical_features.items():
+            vocab_size = feature_config['vocab_size']
+            embed_dim = feature_config['embed_dim']
             
-            self.embeddings[feat_name] = nn.Embedding(vocab_size, embed_dim)
-            total_embed_dim += embed_dim
+            self.embeddings[feature_name] = nn.Embedding(vocab_size, embed_dim)
+            embeddings_dim += embed_dim
             
-            #print(f"{feat_name}: vocab={vocab_size}, embed={embed_dim}")
         
-        # Calculate input dimension
-        num_numerical = len(self.numerical_features)
-        input_dim = num_numerical + total_embed_dim
+        numerical_dims = len(self.numerical_features)
+        input_dim = numerical_dims + embeddings_dim
+        #print(input_dim)
         
-        print(f"\nTotal input dim: {num_numerical} numerical + {total_embed_dim} embedding = {input_dim}")
-        
-        # Build MLP
+
+        # MLP
         self.mlp = nn.Sequential(
+
             nn.Linear(input_dim, 256),
             nn.BatchNorm1d(256),
             nn.ReLU(),
-            nn.Dropout(0.3),
+            #nn.Dropout(0.2),
+
+            nn.Linear(256, 192),
+            nn.BatchNorm1d(192),
+            nn.ReLU(),
             
-            nn.Linear(256, 128),
+            nn.Linear(192, 128),
             nn.BatchNorm1d(128),
             nn.ReLU(),
-            nn.Dropout(0.3),
-            
-            nn.Linear(128, 64),
+            #nn.Dropout(0.15),
+
+            nn.Linear(128, 96),
+            nn.BatchNorm1d(96),
+            nn.ReLU(),
+
+            nn.Linear(96, 64),
             nn.BatchNorm1d(64),
             nn.ReLU(),
-            nn.Dropout(0.3),
             
-            nn.Linear(64, 1)
+            nn.Linear(64, 48),
+            nn.BatchNorm1d(48),
+            nn.ReLU(),
+            #nn.Dropout(0.1),
+
+            nn.Linear(48, 32),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
+            
+            nn.Linear(32, 1)
         )
-        
-        # Store feature order for forward pass
-        self.categorical_order = list(self.categorical_features.keys())
+
         
     def forward(self, X_numerical, X_categorical):
         """
@@ -62,16 +82,32 @@ class MLPCarPriceRegressionNet(nn.Module):
         Returns:
             (batch_size, 1) - log(price) predictions
         """
-        # Process embeddings in the correct order
+        
+        '''
+        embedded_features = [
+            self.embed_manufacturer(X_categorical[:, 0]),
+            self.embed_fuel_type(X_categorical[:, 1]),
+            self.embed_condition(X_categorical[:, 2]),
+            self.embed_body_type(X_categorical[:, 3]),
+            self.embed_color(X_categorical[:, 4]),
+            self.embed_drive_type(X_categorical[:, 5]),
+            self.embed_transmission_type(X_categorical[:, 6])
+        ]
+        '''
         embedded_features = []
-        for i, feat_name in enumerate(self.categorical_order):
-            embedded = self.embeddings[feat_name](X_categorical[:, i])
+        for i, feature_name in enumerate(self.categorical_order):
+            embedded = self.embeddings[feature_name](X_categorical[:, i])
             embedded_features.append(embedded)
         
-        # Concatenate everything
         combined = torch.cat([X_numerical] + embedded_features, dim=1)
-        
-        # Pass through MLP
+
         output = self.mlp(combined)
-        
         return output
+    
+
+if __name__ == "__main__":
+    model = MLPCarPriceRegressionNet_V1('config/preprocessor_config.pkl')
+
+    print(f"{sum(p.numel() for p in model.parameters())} parameters")
+    
+
